4c991ef5553fa1eaf641cac2c40cf9b2584f20a1
LouiseMillan <milouisedu@gmail.com>
2022-05-07 15:07:01 -0500

    Sprint 1 - requerimiento 3 y 4

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -10,7 +10,30 @@ describe("Unit Tests for the User class", () => {
         expect(user.bio).toBe("Bio");
         expect(user.dateCreated).not.toBeUndefined();
         expect(user.dateCreated).not.toBeNull();
-        expect(user.lastUpdate).not.toBeUndefined();
-        expect(user.lastUpdate).not.toBeNull();
+        expect(user.lastUpdated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeNull();
+    });
+
+    test("Add getters", () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio");
+
+        expect(user.getUsername).toBe("carlogilmar");
+        expect(user.getBio).toBe("Bio");
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getDateCreated).not.toBeNull();
+        expect(user.getLastUpdated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeNull();
+    });
+
+    test("Add setters", () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio");
+
+        user.setUsername = "Gilmar";
+        expect(user.username).toBe("Gilmar");
+        expect(user.getUsername).toBe("Gilmar");
+
+        user.setBio = "New bio";
+        expect(user.bio).toBe("New bio");
+        expect(user.getBio).toBe("New bio");
     });
 });
