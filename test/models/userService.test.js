a847163ca73a45db03bec8d391b2097fd99dfd09
LouiseMillan <milouisedu@gmail.com>
2022-05-07 15:57:28 -0500

    Sprint 2 - requerimiento 2

--- test/models/userService.test.js
+++ test/models/userService.test.js
@@ -10,4 +10,15 @@ describe("Test for UserServices", () => {
         expect(user.bio).not.toBeUndefined();
         expect(user.bio).not.toBeNull();
     });
+
+    test("2. Get all user data un a list", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo");
+
+        const userInfoList = UserService.getInfo(user);
+        
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe("carlogilmar");
+        expect(userInfoList[2]).toBe("Carlo");
+        expect(userInfoList[3]).toBe("Sin bio");
+    });
 });
